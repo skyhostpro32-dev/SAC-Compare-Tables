cacebd6bd06aeed324ff9af96468b3fa83c01e24
skyhostpro32-dev <skyhostpro32@gmail.com>
2026-05-28 22:10:41 +0530

    Update app.py

--- app.py
+++ app.py
@@ -36,7 +36,7 @@ load_css()
 st.markdown("""
 <div class="top-bar">
     <div>
-        <h1> <img 
+        <h1>   <img 
         src="https://upload.wikimedia.org/wikipedia/commons/5/59/SAP_2011_logo.svg"
         width="70"
     >
